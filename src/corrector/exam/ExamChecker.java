b1e40d12273b03c3f3b5283c49c5ac848599837c
IXRAHEEM <ebrahemebrahem401@gmail.com>
2024-12-10 22:01:46 +0300

    edit imports

--- src/corrector/exam/ExamChecker.java
+++ src/corrector/exam/ExamChecker.java
@@ -1,6 +1,10 @@
 package corrector.exam;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.HashSet;
 
 public class ExamChecker {
     private List<Character> correctAnswers;
